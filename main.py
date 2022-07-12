25bc3ce98e925b72ba81b8a0ae30ea7b381c8b25
Arrackisarookie <arrackisarookie@gmail.com>
2022-07-12 21:54:58 +0800

    Version0.0.2

--- main.py
+++ main.py
@@ -3,124 +3,126 @@ import sys
 import random
 
 CAPTION = 'Snaaaaaaaake~'
-WIDTH = 640
-HEIGHT = 480
-FPS = 30
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+SCREEN_FPS = 30
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-SPEED = 1
+
+SNAKE_INIT_LENGTH = 10
+SNAKE_INIT_WIDTH = 10
+SNAKE_SPEED = 2
+
+UP = 1
+DOWN = -1
+LEFT = 2
+RIGHT = -2
+
+DIRECTIONS = [UP, DOWN, LEFT, RIGHT]
 
 
 class Snake(pygame.sprite.Sprite):
+
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.first_x = random.random() * WIDTH
-        self.first_y = random.random() * HEIGHT
-        self.image = pygame.Surface((10, 5))
+        self.first_x = random.randint(0, SCREEN_WIDTH)
+        self.first_y = random.randint(0, SCREEN_HEIGHT)
+        self.first_direction = random.choice(DIRECTIONS)
+        self.speed = SNAKE_SPEED
+
+        self.image = pygame.Surface((SNAKE_INIT_LENGTH, SNAKE_INIT_WIDTH))
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.reset()
 
-        self.speed_x = 0
-        self.speed_y = 0
-
-    def control(self, x, y):
-        self.speed_x += x
-        self.speed_y += y
+    def changeDirection(self, direction):
+        if self.direction + direction != 0:
+            self.direction = direction
 
     def update(self):
-        self.rect.x += self.speed_x
-        self.rect.y += self.speed_y
+        if self.direction == UP:
+            self.rect.y -= self.speed
+        elif self.direction == DOWN:
+            self.rect.y += self.speed
+        elif self.direction == LEFT:
+            self.rect.x -= self.speed
+        elif self.direction == RIGHT:
+            self.rect.x += self.speed
 
         # 边界监测
-        if self.rect.right > WIDTH:
-            self.rect.right = WIDTH
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
         elif self.rect.left < 0:
             self.rect.left = 0
 
-        if self.rect.bottom > HEIGHT:
-            self.rect.bottom = HEIGHT
+        if self.rect.bottom > SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
         elif self.rect.top < 20:
             self.rect.top = 20
 
     def reset(self):
         self.rect.x = self.first_x
         self.rect.y = self.first_y
+        self.direction = self.first_direction
 
 
-class SpeedLabel(pygame.sprite.Sprite):
+class Billboard(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.content = 'speed: , position:'
+        self.content = ''
         self.font = pygame.font.SysFont('arial', 20)
-        self.image = self.font.render(self.content, True, (255, 0, 0), BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH-30, 10)
+        self.update()
 
-    def updateLabel(self, speed, position):
-        self.content = 'speed: {}, position: {}'.format(speed, position)
+    def rewrite(self, content):
+        self.content = content
+
+    def update(self):
         self.image = self.font.render(self.content, True, (255, 0, 0), BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = 0
         self.rect.y = 0
-
+        
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption(CAPTION)
 clock = pygame.time.Clock()
 
 sprites = pygame.sprite.Group()
 snake = Snake()
-speedLabel = SpeedLabel()
+billboard = Billboard()
 sprites.add(snake)
-sprites.add(speedLabel)
+sprites.add(billboard)
 
-
-info = 'speed_xy: ({}, {})'
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-        # 油门
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_w:
-                snake.control(0, -SPEED)
+                snake.changeDirection(UP)
                 print('UP')
-            if event.key == pygame.K_a:
-                snake.control(-SPEED, 0)
-                print('LEFT')
             if event.key == pygame.K_s:
-                snake.control(0, SPEED)
+                snake.changeDirection(DOWN)
                 print('DOWN')
+            if event.key == pygame.K_a:
+                snake.changeDirection(LEFT)
+                print('LEFT')
             if event.key == pygame.K_d:
-                snake.control(SPEED, 0)
+                snake.changeDirection(RIGHT)
                 print('RIGHT')
 
             if event.key == pygame.K_r:
                 snake.reset()
                 print('Reset')
 
-        # 刹车
-        if event.type == pygame.KEYUP:
-
-            if event.key == pygame.K_w:
-                snake.control(0, -SPEED)
-                print('UP STOP')
-            if event.key == pygame.K_a:
-                snake.control(-SPEED, 0)
-                print('LEFT STOP')
-            if event.key == pygame.K_s:
-                snake.control(0, SPEED)
-                print('DOWN STOP')
-            if event.key == pygame.K_d:
-                snake.control(SPEED, 0)
-                print('RIGHT STOP')
-
     # Update
-    speedLabel.updateLabel((snake.speed_x, snake.speed_y), (snake.rect.top, snake.rect.bottom, snake.rect.left, snake.rect.right))
+    slogan = 'Position: ({}, {}, {}, {})'
+    billboard.rewrite(slogan.format(snake.rect.top, snake.rect.bottom, snake.rect.left, snake.rect.right))
     sprites.update()
 
     # Draw / render
@@ -128,4 +130,4 @@ while True:
     sprites.draw(screen)
 
     pygame.display.flip()
-    clock.tick(FPS)
+    clock.tick(SCREEN_FPS)
