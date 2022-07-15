b727ababa37f5aa66c0dbd257f59234f93a014f1
Arrackisarookie <arrackisarookie@gmail.com>
2022-07-15 15:10:53 +0800

    [A] 新增了子弹，玩家可以按空格发射子弹，攻击小怪

--- main.py
+++ main.py
@@ -38,6 +38,22 @@ class Mob(pygame.sprite.Sprite):
             self.speed_x = random.randrange(-3, 3)
 
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speed_y = -10
+
+    def update(self):
+        self.rect.y += self.speed_y
+        if self.rect.bottom < 0:
+            self.kill()
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -47,6 +63,11 @@ class Player(pygame.sprite.Sprite):
         self.rect.centerx = WIDTH / 2
         self.rect.bottom = HEIGHT - 10
         self.speed_x = 0
+
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
     def update(self):
         self.speed_x = 0
@@ -65,38 +86,46 @@ class Player(pygame.sprite.Sprite):
             self.rect.left = 0
 
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption(CAPTION)
-    clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption(CAPTION)
+clock = pygame.time.Clock()
 
-    all_sprites = pygame.sprite.Group()
-    player = Player()
-    all_sprites.add(player)
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 
-    mobs = pygame.sprite.Group()
-    for i in range(8):
+bullets = pygame.sprite.Group()
+
+mobs = pygame.sprite.Group()
+for i in range(8):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+
+    all_sprites.update()
+    hits = pygame.sprite.groupcollide(bullets, mobs, True, True)
+    for hit in hits:
         m = Mob()
         all_sprites.add(m)
         mobs.add(m)
 
-    running = True
-    while running:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
 
-        all_sprites.update()
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
+    pygame.display.flip()
 
-        pygame.display.flip()
-
-    pygame.quit()
-
-
-
-if __name__ == '__main__':
-    main()
+pygame.quit()
